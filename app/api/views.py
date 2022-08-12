42a1f85537ff54bad48454b5c514daca82c4bc1e
ampaking <78628177+ampaking@users.noreply.github.com>
2022-08-12 17:04:45 +0900

    feat: set data stracture

--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from itertools import count
 from django.http import HttpResponse
@@ -6,7 +7,6 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 API_URL = 'https://covid-19.dataflowkit.com/v1/'
-FLAG_URL = 'https://countryflagsapi.com/svg/'
 
 
 def Home(request):
@@ -30,15 +30,27 @@ class CountryView(generics.ListAPIView):
     def get(self, request, country, *args, **kwargs):
         # get county name and make url path
         url = f'{API_URL}{country.lower()}'
-        country_flag_url = f'{FLAG_URL}{country.lower()}'
         data = requests.get(url)
-        flag = requests.get(country_flag_url)
+        data_format = []
         try:
             # make data to json format for better readability
             data = data.json()
+
+            data_format(
+                {
+                    "active_cases_text": data["Active Cases_text"],
+                    "country_text": data["Country_text"],
+                    "last_update": data["Last Update"],
+                    "new_cases_text": data["New Cases_text"],
+                    "new_deaths_text": data["New Deaths_text"],
+                    "total_cases_text": data["Total Cases_text"],
+                    "total_deaths_text": data["Total Deaths_text"],
+                    "total_recovered_text": data["Total Recovered_text"]
+                }
+            )
         except:
             return Response({"Error: Please visit after some time later"})
-        returning_data = {"data": data, "flag": flag}
+        returning_data = {"data": data_format}
         return Response(returning_data)
 
 
@@ -46,10 +58,22 @@ class GlobalView(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         url = f'{API_URL}world'
         data = requests.get(url)
+        data_format = []
         try:
-            # make data to json format
             data = data.json()
-            print(data)
+            data_format.append(
+                {
+                    "active_cases_text": data["Active Cases_text"],
+                    "country_text": data["Country_text"],
+                    "last_update": data["Last Update"],
+                    "new_cases_text": data["New Cases_text"],
+                    "new_deaths_text": data["New Deaths_text"],
+                    "total_cases_text": data["Total Cases_text"],
+                    "total_deaths_text": data["Total Deaths_text"],
+                    "total_recovered_text": data["Total Recovered_text"]
+                }
+            )
         except:
             return Response({"Error: Please visit after some time later"})
-        return Response(data)
+        returning_data = {"data": data_format}
+        return Response(returning_data)
